2827ba3b15895342173aa634eab2077db041de0a
matan-sarisrael <128925037+matan-sarisrael@users.noreply.github.com>
2023-07-22 08:07:48 -0700

    Update assn3_tests.py

--- tests/assn3_tests.py
+++ tests/assn3_tests.py
@@ -1,132 +1,147 @@
-from TestUtils import RestApiAgent, Assertions
+import requests
+import json
 
 
-def test_create_new_dishes():
-    """
-    [TEST 1]
-    Execute three POST /dishes requests using the dishes, “orange”, “spaghetti”, and “apple pie”.
-    The test is successful if:
-        1. All 3 requests return unique IDs (none of the IDs are the same).
-        2. The return status code from each POST request is 201.
-    """
-    DISHES_TO_CREATE = ["orange", "spaghetti", "apple pie"]
-    responses = []
+class ConnectionController:
 
-    for dish in DISHES_TO_CREATE:
-        response = RestApiAgent.add_dish(dish)
-        Assertions.assert_valid_added_resource(response)
-        responses.append(response)
+    URL = "http://127.0.0.1:8000"
 
-    Assertions.assert_unique_resource_id(responses)
+    @staticmethod
+    def http_get(resource: str):
+        response = requests.get(url=f"{ConnectionController.URL}/{resource}", headers={"Content-Type": "application/json"})
+        return response
 
+    @staticmethod
+    def http_delete(resource: str):
+        response = requests.delete(url=f"{ConnectionController.URL}/{resource}", headers={"Content-Type": "application/json"})
+        return response
 
-def test_orange_dish_sanity():
-    """
-    [TEST 2]
-    Execute a GET dishes/<orange-ID> using the ID of the orange dish.
-    The test is successful if:
-        1. The sodium field of the return JSON object is between .9 and 1.1.
-        2. The return status code from the request is 200.
-    """
-    ORANGE_ID = 1
-    orange_resource = RestApiAgent.get_dish_by_id(ORANGE_ID)
+    @staticmethod
+    def http_post(resource: str, data: {}):
+        response = requests.post(url=f"{ConnectionController.URL}/{resource}", headers={"Content-Type": "application/json"},
+                                 data=json.dumps(data))
+        return response
 
-    Assertions.assert_status_code(response=orange_resource, status_code=200)
-    Assertions.assert_field_in_range(response=orange_resource, field="sodium", val_range=(0.9, 1.1))
+    @staticmethod
+    def http_put(resource: str, data: {}):
+        response = requests.put(url=f"{ConnectionController.URL}/{resource}", headers={"Content-Type": "application/json"},
+                                data=json.dumps(data))
+        return response
 
+    @staticmethod
+    def post_raw(resource: str, data: {}, headers: {}):
+        response = requests.post(url=f"{ConnectionController.URL}/{resource}", headers=headers, data=json.dumps(data))
+        return response
 
-def test_get_all_dishes_sanity():
-    """
-    [TEST 3]
-    Execute a GET /dishes request.
-    The test is successful if:
-        1. The returned JSON object has 3 embedded JSON objects (dishes)
-        2. The return status code from the GET request is 200.
-    """
-    EXPECTED_NUM_DISHES = 3
-    all_dishes = RestApiAgent.get_dishes()
+    @staticmethod
+    def add_dish(name: str) -> int:
+        dish = {"name": name}
+        response = ConnectionController.http_post("dishes", dish)
+        Assertion.assert_valid_added_resource(response)
+        return response.json()
 
-    assert len(all_dishes.json()) == EXPECTED_NUM_DISHES
-    Assertions.assert_status_code(response=all_dishes, status_code=200)
-
-
-def test_add_non_exists_dish():
-    """
-    [TEST 4]
-    Execute a POST /dishes request supplying the dish name “blah”.
-    The test is successful if:
-        1. The return value is -3
-        2. The return code is 404 or 400 or 422.
-    """
-    NON_EXIST_DISH_NAME = "blah"
-    POSSIBLE_SATUS_CODES = [400, 404, 422]
-    EXPECTED_RETURN_VALUE = -3
-    non_exists_response = RestApiAgent.add_dish(NON_EXIST_DISH_NAME)
-
-    Assertions.assert_return_value(response=non_exists_response, returned_value=EXPECTED_RETURN_VALUE)
-    Assertions.assert_status_code(response=non_exists_response, status_code=POSSIBLE_SATUS_CODES)
+    @staticmethod
+    def add_meal(name: str, appetizer_id: int, main_id: int, dessert_id: int) -> int:
+        meal = {
+            "name": name,
+            "appetizer": appetizer_id,
+            "main": main_id,
+            "dessert": dessert_id
+        }
+        response = ConnectionController.http_post("meals", meal)
+        Assertion.assert_valid_added_resource(response)
+        assert response.json() > 0
+        return response.json()
 
 
-def test_add_already_exists_dish():
-    """
-    [TEST 5]
-    Perform a POST dishes request with the dish name “orange”.
-    The test is successful if:
-        1. The return value is -2 (same dish name as existing dish)
-        2. The return status code is 400 or 404 or 422.
-    """
+class Assertion:
+
+    @staticmethod
+    def assert_ret_value(response: requests.Response, returned_value: any):
+        assert response.json() == returned_value
+
+    @staticmethod
+    def assert_err_code(response: requests.Response, error_code: int):
+        assert response.status_code == error_code
+
+    @staticmethod
+    def assert_valid_added_resource(response: requests.Response):
+        assert response.status_code == 201
+
+
+orange_dish_id: int = None
+spaghetti_dish_id: int = None
+apple_pie_dish_id: int = None
+
+
+def test_1():
+    global orange_dish_id, apple_pie_dish_id, spaghetti_dish_id
+    orange_dish_id = ConnectionController.add_dish("orange")
+    spaghetti_dish_id = ConnectionController.add_dish("spaghetti")
+    apple_pie_dish_id = ConnectionController.add_dish("apple pie")
+    assert orange_dish_id != spaghetti_dish_id
+    assert orange_dish_id != apple_pie_dish_id
+    assert spaghetti_dish_id != apple_pie_dish_id
+
+
+def test_2():
+    global orange_dish_id
+    assert orange_dish_id is not None
+
+    response = ConnectionController.http_get(f"dishes/{orange_dish_id}")
+    Assertion.assert_err_code(response, error_code=200)
+
+    orange_sodium = response.json()["sodium"]
+    assert 0.9 <= orange_sodium <= 1.1
+
+
+def test_3():
+    response = ConnectionController.http_get("dishes")
+    Assertion.assert_err_code(response, error_code=200)
+
+    dishes = response.json()
+    assert len(dishes) == 3
+
+
+def test_4():
+    INVALID_DISH = {"name": "blah"}
+    response = ConnectionController.http_post("dishes", INVALID_DISH)
+    Assertion.assert_ret_value(response, -3)
+    assert response.status_code == 404 or response.status_code == 400 or response.status_code == 422
+
+
+def test_5():
     DISH_NAME = "orange"
-    POSSIBLE_SATUS_CODES = [400, 404, 422]
-    EXPECTED_RETURN_VALUE = -2
-    response = RestApiAgent.add_dish(DISH_NAME)
-
-    Assertions.assert_return_value(response=response, returned_value=EXPECTED_RETURN_VALUE)
-    Assertions.assert_status_code(response=response, status_code=POSSIBLE_SATUS_CODES)
+    response = ConnectionController.http_post("dishes", {"name": DISH_NAME})
+    Assertion.assert_ret_value(response, -2)
+    assert response.status_code == 404 or response.status_code == 400 or response.status_code == 422
 
 
-def test_create_meal():
-    """
-    [TEST 6]
-    Perform a POST /meals request specifying that the meal name is “delicious” s.t.
-        { name: "delicious", appetizer: 1 (orange id), main: 2 (spaghetti id), dessert: 3 (apple pie id) }
-    The test is successful if:
-        1. The returned value ID > 0
-        2. The return status code is 201.
-    """
-    MEAL_NAME = "delicious"
-    create_meal_response = RestApiAgent.add_meal(name=MEAL_NAME, appetizer_id=1, main_id=2, dessert_id=3)
+def test_6():
+    global orange_dish_id, apple_pie_dish_id, spaghetti_dish_id
+    assert orange_dish_id is not None
+    assert apple_pie_dish_id is not None
+    assert spaghetti_dish_id is not None
 
-    Assertions.assert_resource_id_positive_int(create_meal_response)
-    Assertions.assert_valid_added_resource(create_meal_response)
+    ConnectionController.add_meal("delicious", orange_dish_id, spaghetti_dish_id, apple_pie_dish_id)
 
 
-def test_get_all_meals_sanity():
-    """
-    [TEST 7]
-    Perform a GET /meals request.
-    The test is successful if:
-        1. The returned JSON object has 1 meal.
-        2. The return status code is 200.
-    """
-    EXPECTED_NUM_MEALS = 1
-    all_meals = RestApiAgent.get_meals()
+def test_7():
+    response = ConnectionController.http_get("meals")
+    Assertion.assert_err_code(response, error_code=200)
+    meals = response.json()
+    assert len(meals) == 1
 
-    assert len(all_meals.json()) == EXPECTED_NUM_MEALS
-    Assertions.assert_status_code(response=all_meals, status_code=200)
+    for key in meals:
+        assert 400 <= meals[key]["cal"] <= 500
 
 
-def test_add_already_exists_meal():
-    """
-    [TEST 8]
-    Perform a POST /meals request as in test 6 with the same meal name (and courses can be the same or different)
-    The test is successful if:
-        1. The returned value code is -2 (same meal name as existing meal)
-        2. The return status code is 400 or 422.
-    """
-    MEAL_NAME = "delicious"
-    POSSIBLE_SATUS_CODES = [400, 422]
-    EXPECTED_RETURN_VALUE = -2
-    already_exists_meal_response = RestApiAgent.add_meal(name=MEAL_NAME, appetizer_id=1, main_id=2, dessert_id=3)
-
-    Assertions.assert_status_code(response=already_exists_meal_response, status_code=POSSIBLE_SATUS_CODES)
-    Assertions.assert_return_value(response=already_exists_meal_response, returned_value=EXPECTED_RETURN_VALUE)
+def test_8():
+    global orange_dish_id, apple_pie_dish_id, spaghetti_dish_id
+    assert orange_dish_id is not None
+    assert apple_pie_dish_id is not None
+    assert spaghetti_dish_id is not None
+    meal = {"name": "delicious", "appetizer": orange_dish_id, "main": spaghetti_dish_id, "dessert": apple_pie_dish_id}
+    response = ConnectionController.http_post("meals", meal)
+    Assertion.assert_ret_value(response, returned_value=-2)
+    assert response.status_code == 400 or response.status_code == 422
